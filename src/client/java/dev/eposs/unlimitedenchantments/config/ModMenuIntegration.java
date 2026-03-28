cdabc1ce78696f77c90d2b4ef50bbc1580e96092
Eposs <123678741+Eposs10@users.noreply.github.com>
2026-03-28 20:39:08 +0100

    Mc 26.1 (#1)

--- src/client/java/dev/eposs/unlimitedenchantments/config/ModMenuIntegration.java
+++ src/client/java/dev/eposs/unlimitedenchantments/config/ModMenuIntegration.java
@@ -2,13 +2,11 @@ package dev.eposs.unlimitedenchantments.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
     }
 }
